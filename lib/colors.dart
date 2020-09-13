9eb4731f306fa7f1f210043e8735782d21ce67de
DanielBarragan96 <ie702554@iteso.mx>
2020-09-13 17:07:10 -0500

    se corrige el tipoo de variables a const Color

--- lib/colors.dart
+++ lib/colors.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 
-const kDarkOrange = const Color(0xFFED9665);
-const kLightOrange = const Color(0xFFF9B784);
-const kLightBrown = const Color(0xFFBCB0A1);
-const kDrakBrown = const Color(0xFF8B8175);
-const kDarkBlue = const Color(0xFF214254);
-const kBlack = const Color(0xFF121B22);
-const kWhite = const Color(0xFFFFFFFF);
+const Color kDarkOrange = const Color(0xFFED9665);
+const Color kLightOrange = const Color(0xFFF9B784);
+const Color kLightBrown = const Color(0xFFBCB0A1);
+const Color kDrakBrown = const Color(0xFF8B8175);
+const Color kDarkBlue = const Color(0xFF214254);
+const Color kBlack = const Color(0xFF121B22);
+const Color kWhite = const Color(0xFFFFFFFF);
