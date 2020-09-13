8d886f2ff4bf4e5aa1bb36067c113335881df2f5
DanielBarragan96 <ie702554@iteso.mx>
2020-09-13 00:07:57 -0500

    se crea product dessert

--- lib/models/product_dessert.dart
+++ lib/models/product_dessert.dart
@@ -1,2 +1,39 @@
 // TODO: Crear producto "ProductCup" siguiendo la logica de Drinks y Grains
 // TODO: Agregar al ProductRepository una lista de estos productos.
+import 'dart:math';
+
+import 'package:flutter/foundation.dart';
+
+enum ProductSlice { HALF, SINGLE, DOUBLE }
+
+class ProductDessert {
+  final String productTitle; // nombre del producto
+  final String productDescription; // descripcion del producto
+  final String productImage; // url de imagen del producto
+  ProductSlice productSlice; // tamano del producto
+  double productPrice; // precio del producto autocalculado
+  final int productAmount; // cantidad de producto por comprar
+  final bool liked;
+
+  ProductDessert({
+    @required this.productTitle,
+    @required this.productDescription,
+    @required this.productImage,
+    @required this.productSlice,
+    @required this.productAmount,
+    this.liked = false,
+  }) {
+    // inicializa el precio de acuerdo al weight del producto
+    productPrice = productPriceCalculator();
+  }
+
+  double productPriceCalculator() {
+    if (this.productSlice == ProductSlice.HALF)
+      return (40 + Random().nextInt(40)).toDouble();
+    if (this.productSlice == ProductSlice.SINGLE)
+      return (60 + Random().nextInt(60)).toDouble();
+    if (this.productSlice == ProductSlice.DOUBLE)
+      return (80 + Random().nextInt(80)).toDouble();
+    return 9999.0;
+  }
+}
