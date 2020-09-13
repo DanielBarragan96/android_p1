2b908ebcfad0791f31bc424102410d77bc9996e5
DanielBarragan96 <ie702554@iteso.mx>
2020-09-13 16:51:17 -0500

    se crea archivo con los colores de la app

---
+++ lib/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const kDarkOrange = const Color(0xFFED9665);
+const kLightOrange = const Color(0xFFF9B784);
+const kLightBrown = const Color(0xFFBCB0A1);
+const kDrakBrown = const Color(0xFF8B8175);
+const kDarkBlue = const Color(0xFF214254);
+const kBlack = const Color(0xFF121B22);
+const kWhite = const Color(0xFFFFFFFF);
