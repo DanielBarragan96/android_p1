750cad7ddba8cd6183405a4a09dba095b0a2f983
DanielBarragan96 <ie702554@iteso.mx>
2020-09-15 14:05:48 -0500

    se agrega atributo size para items de carrito

--- lib/models/product_item_cart.dart
+++ lib/models/product_item_cart.dart
@@ -3,6 +3,7 @@ import 'package:estructura_practica_1/models/product_repository.dart';
 
 class ProductItemCart {
   String productTitle;
+  String productSize;
   int productAmount;
   double productPrice;
   String productImage;
@@ -11,6 +12,7 @@ class ProductItemCart {
   ProductItemCart({
     this.typeOfProduct,
     @required this.productTitle,
+    @required this.productSize,
     @required this.productAmount,
     @required this.productPrice,
     @required this.productImage,
