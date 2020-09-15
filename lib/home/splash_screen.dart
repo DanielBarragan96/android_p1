743b171a253f8c49b4ee1479deef9945f08ca589
DanielBarragan96 <ie702554@iteso.mx>
2020-09-15 09:36:53 -0500

    se implementa splash_screen en app

---
+++ lib/home/splash_screen.dart
@@ -0,0 +1,63 @@
+import 'package:estructura_practica_1/login/main_login.dart';
+import 'package:flutter/material.dart';
+
+import '../colors.dart';
+
+class SplashScreen extends StatefulWidget {
+  SplashScreen({Key key}) : super(key: key);
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    _mockCheckForSession().then((status) {
+      if (status) {
+        Navigator.of(context).pushReplacement(
+          MaterialPageRoute(
+            builder: (BuildContext context) => MainLoginPage(),
+          ),
+        );
+      }
+    });
+  }
+
+  Future<bool> _mockCheckForSession() async {
+    await Future.delayed(Duration(seconds: 5), () {});
+    return true;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: kDrakBrown,
+      body: SafeArea(
+        child: Center(
+          child: Stack(children: [
+            Container(
+              height: MediaQuery.of(context).size.height,
+              child: Image.asset(
+                "coffee.jpg",
+                fit: BoxFit.cover,
+              ),
+            ),
+            Center(
+              child: Container(
+                height: 180,
+                width: 180,
+                child: Image.asset(
+                  "cupping.png",
+                  fit: BoxFit.fitWidth,
+                ),
+              ),
+            ),
+          ]),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:estructura_practica_1/home/splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:estructura_practica_1/utils/constants.dart';
 
@@ -9,13 +10,16 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    // return MaterialApp(
+    //   title: APP_TITLE,
+    //   theme: ThemeData(
+    //     primarySwatch: PRIMARY_COLOR,
+    //   ),
+    //   // home: Home(title: APP_TITLE),
+    //   home: MainLoginPage(),
+    // );
     return MaterialApp(
-      title: APP_TITLE,
-      theme: ThemeData(
-        primarySwatch: PRIMARY_COLOR,
-      ),
-      // home: Home(title: APP_TITLE),
-      home: MainLoginPage(),
+      home: SplashScreen(),
     );
   }
 }
